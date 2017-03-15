d5aadb880643c0f0ab8775525da0921a2e774590
刘亚飞 <liuyafei@fdc.com.cn>
2017-03-15 23:39:52 +0800

    commit

---
+++ content/docker_install.md
@@ -0,0 +1,14 @@
+Title: docker(一) 安装  
+Date: 2015-01-10 10:30  
+Modified: 2015-01-10 10:30   
+Category: 虚拟化    
+Tags: puppet   
+Slug: docker_install   
+Author: Kevin
+
+# 安装和使用docker #
+介绍：
+![docker](/img/docker.png)
+Docker 是一个开源的应用容器引擎，让开发者可以打包他们的应用以及依赖包到一个可移植的容器中，然后发布到任何流行的 Linux 机器上，也可以实现虚拟化。容器是完全使用沙箱机制，相互之间不会有任何接口（类似 iPhone 的 app）。几乎没有性能开销,可以很容易地在机器和数据中心中运行。最重要的是,他们不依赖于任何语言、框架或包装系统。
+
+不要忘本。 最重要是变成自己的知识，而
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,42 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-
+# Site settings
 AUTHOR = u'Kevin'
-SITENAME = u'Feis Blog'
-SITEURL = ''
+SITENAME = u"Kevin's Blog"
+SITEURL = 'http://www.feisblog.com'
 
+SITESUBTITLE = '专注自动化运维、大数据、云计算'
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = u'zh'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+#TRANSLATION_FEED_ATOM = None
+#AUTHOR_FEED_ATOM = None
+#AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#添加订阅等功能
+SOCIAL = (
+		   ('RSS','http://feisblog.com/feeds/all.atom.xml'),
+          )
 
 DEFAULT_PAGINATION = 10
-THEME = 'pelican-themes/gum'
+#模板配置
+
+THEME = 'pelican-themes/bootstrap2'
 DISQUS_SITENAME = u'feisblog'
 #URL配置
 ARTICLE_URL = 'pages/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'pages/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+##设置菜单项
+MENUITEMS = (
+            
+            )
+###添加静态文件			
+STATIC_PATHS = ['img']
+
 
 #Pelican 插件
 PLUGIN_PATH = 'pelican-plugins'     #设置路径
@@ -55,3 +69,18 @@ SITEMAP = {
         'pages': 'monthly'
     }
 }
+
+
+
+#这个是farseerfc同学自己加的, 可以显示他的新浪微博内容, 有微博的话可以把这些加上~
+#~ TWITTER_USERNAME = 'farseerfc'
+#~ SIDEBAR_CUSTOM = r"""
+#~ <li class="nav-header"><h4><i class="icon-list-alt"></i>Weibo</h4></li>
+#~ <iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" 
+#~ src="<http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=1&ptype=1&speed=0&skin=2&isTitle=1&noborder=1&isWeibo=1&isFans=1&uid=1862842353&verifier=b193b9de&dpc=1>">
+#~ </iframe>
+#~ """
+
+#google自定义搜索(大概是站内搜索吧)
+#~ GOOGLE_CUSTOM_SEARCH_SIDEBAR = "001578481551708017171:axpo6yvtdyg"
+#~ GOOGLE_CUSTOM_SEARCH_NAVBAR = "001578481551708017171:hxkva69brmg"
